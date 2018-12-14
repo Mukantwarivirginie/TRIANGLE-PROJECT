3712832faccac6520845c7da9ab267ca7c61c4ab
Wecode <Mukantwarivirginie>
2018-12-14 13:13:44 +0200

    creat paragraph

--- triangle.js
+++ triangle.js
@@ -1,5 +1,8 @@
 function myFunction(){
-    var firstString=document.getElementById("side2").value;
+    var sideone=document.getElementById("sideone").value;
+  alert(sideone);
+  var sidetwo=document.getElementById("sidetwo").value;
   alert(firstString);
- 
+  var sidethree=document.getElementById("sidethree").value;
+  alert(sidethree);
  };
